f15006e284691996f579b89aaf7001fe0281de27
damiano <damianodeorazi@hotmail.com>
2025-11-20 12:15:19 +0100

    aggiunto codice user e modifica file make

---
+++ user.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+#define SIZE 256
+#define AUDIT if (1)
+
+char *device_path = "./SINGLEFILE-FS/image";
+char *password = "correct_password";
+
+int main() {
+    
+    char *device_name = realpath(device_path, NULL);
+    if (device_name == NULL) {
+        perror("realpath failed");
+        return EXIT_FAILURE;
+    }
+
+    int ret = activate_snapshot(device_name, password);
+    if (ret == 0) {
+        printf("Failed to activate snapshot for device %s\n", device_name);
+        return EXIT_FAILURE;
+    }
+
+    printf("Snapshot activated successfully for device %s\n", device_name);
+
+    int fd = open("./SINGLEFILE-FS/mount/the-file", O_RDWR);
+    if (fd < 0) {
+        perror("Failed to open the file");
+        return EXIT_FAILURE;
+    }
+
+    char *read_data = malloc(SIZE);
+    ssize_t bytes_read = read(fd, read_data, SIZE);
+    if (bytes_read < 0) {
+        perror("Failed to read from the file");
+        return EXIT_FAILURE;    
+    }
+
+    printf("Read non-modified data: %s\n", read_data);
+
+    // attivare/disattivare per il testing dello snapshot durante le operazioni di scrittura
+    AUDIT {
+        ret = deactivate_snapshot(device_name, password);
+        if (ret == 0) {
+            printf("Failed to deactivate snapshot for device %s\n", device_name);
+            return EXIT_FAILURE;
+        }
+        printf("Snapshot deactivated successfully for device %s\n", device_name);
+    }
+
+    const char *data = "Data written to the file to test snapshot functionality.\n";
+    ssize_t bytes_written = write(fd, data, strlen(data));
+    if (bytes_written < 0) {
+        perror("Failed to write to the file");
+        return EXIT_FAILURE;
+    }
+
+    printf("Wrote %zd bytes to the file.\n", bytes_written);
+
+    ssize_t bytes_read = read(fd, read_data, SIZE);
+    if (bytes_read < 0) {
+        perror("Failed to read from the file");
+        return EXIT_FAILURE;    
+    }
+
+    printf("Read modified data: %s\n", read_data);
+
+    ret = restore_snapshot(device_name, password);
+    if (ret == 0) {
+        printf("Failed to restore snapshot for device %s\n", device_name);
+        return EXIT_FAILURE;     
+    } 
+
+    printf("Snapshot restored successfully for device %s\n", device_name);
+
+    ssize_t bytes_read = read(fd, read_data, SIZE);
+    if (bytes_read < 0) {
+        perror("Failed to read from the file");
+        return EXIT_FAILURE;    
+    }
+
+    printf("Read data after snapshot restoration: %s\n", read_data);
+
+    ret = deactivate_snapshot(device_name, password);
+    if (ret == 0) {
+        printf("Failed to deactivate snapshot for device %s\n", device_name);
+        return EXIT_FAILURE;
+    }
+
+
+}
